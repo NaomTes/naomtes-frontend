cc6feddf545e6219752ad74be4af2c16a80ad4f6
Zohaib Butt <zohaibnadeem13@gmail.com>
2021-04-03 17:26:04 +0500

    changed backend api url

--- library/server.js
+++ library/server.js
@@ -1,4 +1,4 @@
 // api link here
-export const server = "https://activity-management-0.herokuapp.com";
+export const server = "https://warm-cove-76307.herokuapp.com/";
 // export const server = "http://localhost:3000";
 
