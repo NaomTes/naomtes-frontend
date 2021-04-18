7ea64a55e5d3b27dbcce9070192a88680c0c0738
Zohaib Butt <zohaibnadeem13@gmail.com>
2021-04-18 16:05:32 +0500

    aws server url

--- library/server.js
+++ library/server.js
@@ -1,4 +1,5 @@
 // api link here
 // export const server = "https://warm-cove-76307.herokuapp.com/";
-export const server = "https://warm-cove-76307.herokuapp.com/";
+export const server = "http://3.143.225.62:3000/";
+// export const server = "http://localhost:3000";
 // export const server = process.env.SERVER;
